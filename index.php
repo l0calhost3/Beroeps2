4f589ee1ae684d397698135afeaf14513090518e
Yaroslav <supercell.idyarik@gmail.com>
2026-09-24 15:45:05 +0200

    Add files via upload
    Frontend start

--- index.php
+++ index.php
@@ -8,9 +8,52 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/frontend.css">
+    <title>Cook masters</title>
 </head>
 <body>
+    <header>
+        <div class="logo">
+            <img src="images/hat.png" alt="#">
+            <h1>Cook masters</h1>
+        </div>
+        <div class="menu">
+            <a href="">Home</a>
+            <a href="">Recepten</a>
+            <a href="">Over ons</a>
+        </div>
+        <div class="register">
+            <a class="sign-in" href="">sign in</a>
+            <a class="sign-up" href="">sign up</a>
+        </div>
+    </header>
+    <div class="container">
+        <div class="intro">
+            <div class="info">
+               <h2>Lorem ipsum</h2>
+                <h3>Lerom pisum</h3>
+                <p>Lorem Ipsum is simply dummy text of <br>
+                    the printing and typesetting industry. <br>
+                    Lorem Ipsum has been the industry's standard
+                    <br>dummy text ever since 1966,</p>
+                <a href="">Bekijk recepten -></a>
+            </div>
+            <div class="image">
+                <img src="images/image.png" alt="#">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+            <img class="holder" src="images/image2.png" alt="#">
+        <div class="info">
+            <h2>Who we are?</h2>
+            <p>Lorem Ipsum is simply dummy text of <br>
+                the printing and typesetting industry. <br>
+                Lorem Ipsum has been the industry's standard
+                <br>dummy text ever since 1966,</p>
+        </div>
+    </div>
     <form action="./login/"
+
 </body>
 </html>
