ea860f552be67f3ecb7260cefc9cce9c622848e0
104393 <104393@glr.nl>
2026-09-25 11:31:19 +0200

    Work with frontend

--- index.php
+++ index.php
@@ -43,16 +43,51 @@
             </div>
         </div>
     </div>
-    <div class="container">
+    <div class="container, lines">
+        <div class="items">
             <img class="holder" src="images/image2.png" alt="#">
-        <div class="info">
-            <h2>Who we are?</h2>
-            <p>Lorem Ipsum is simply dummy text of <br>
-                the printing and typesetting industry. <br>
-                Lorem Ipsum has been the industry's standard
-                <br>dummy text ever since 1966,</p>
+            <div class="about">
+                <h2>Who we are?</h2>
+                <p>Lorem Ipsum is simply dummy text of the printing
+                    and typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since 1966, when designers at Letraset
+                    and James Mosley, the librarian at St Bride Printing Library in
+                    London, took a 1914 Cicero translation and scrambled it to make dummy
+                    text for Letraset's Body Type sheets. It has survived not only many
+                    decades, but also the leap into electronic typesetting, remaining
+                    essentially unchanged. It was popularised thanks to these sheets and
+                    more recently with desktop publishing software like Aldus PageMaker
+                    and Microsoft Word including versions of Lorem Ipsum.</p>
+         </div>
         </div>
     </div>
+    <div class="container">
+            <div class="about">
+                <h2>Our example</h2>
+                <div class="block">
+                    <div class="block-item-left">
+                        <img src="images/food.png" alt="#">
+                        <div class="text">
+                            <h3>Shrimps</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body </p>
+                        </div>
+                    </div>
+                    <div class="block-item-right">
+                        <div class="text">
+                            <h3>Shrimps</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body </p>
+                        </div>
+                        <img src="images/food2.png" alt="#">
+                    </div>
+                </div>
+            </div>
+    </div>
+    <footer>
+        <div class="end">
+            <p>© [2026] [TEAM 9 Company]. All rights reserved.</p>
+            <a href=""><img src="images/scroll.png" alt="#"></a>
+        </div>
+    </footer>
     <form action="./login/"
 
 </body>
